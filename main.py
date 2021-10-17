eb6c6a92713d83a53eb5ae0821d35dd4b219d4d1
TheWiseWolfy <bogdan_gabriel757@yahoo.com>
2021-10-17 12:31:07 +0300

    Project is fixed

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from PyQt5 import QtWidgets # import PyQt5 widgets
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Create the application object
+app = QtWidgets.QApplication(sys.argv)
 
+# Create the form object
+first_window = QtWidgets.QWidget()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Set window size
+first_window.resize(400, 300)
 
+# Set the form title
+first_window.setWindowTitle("The first pyqt program")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Show form
+first_window.show()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Run the program
+sys.exit(app.exec())
